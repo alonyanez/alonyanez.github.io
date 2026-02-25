787b49dd8ebb4036f7a4e099b8d9bf5b86e6d4f8
J AY <javi.ayanez@gmail.com>
2026-02-25 19:40:39 +0100

    estructura base React con Vite: corrección 1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='main.container'>
+        <section className='column-left'>
+          <h1> Javier Alonso </h1>
+          <p>Full Stack Developer</p>
+        </section>
+
+        <section className='column-right'>
+          <p>Mis primeros inicios con el portfolio</p>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
