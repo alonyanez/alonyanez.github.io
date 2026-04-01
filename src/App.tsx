bde1eff876fcdc7b58aadccf0ed7e9daf8c4e73e
J AY <javi.ayanez@gmail.com>
2026-04-01 12:53:16 +0200

    Corrección enrutado, app y main

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
-import { ProjectDetails } from '@/pages/projects'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
 import Home from '@/pages/Home'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 import Dashboard from '@/pages/Dashboard'
+import TareaForm from './pages/TareaForm'
+import { ProjectDetails } from '@/pages/projects'
 import { authService } from './services/authService'
 import { JSX } from 'react'
-import TareaForm from './pages/TareaForm'
 
 function PrivateRoute({ children }: { children: JSX.Element }) {
   return authService.isAuthenticated() ? children : <Navigate to="/login" />
 }
 
-
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/dashboard' element={<Dashboard/>} />
       <Route path='/login' element={<Login />} />
-      <Route path='/tareas/nueva' element={<TareaForm />} />
       <Route path='/register' element={<Register />} />
+      <Route path='/projects/:slug' element={<ProjectDetails />} />
+
       <Route path='/dashboard' element={
         <PrivateRoute>
           <Dashboard />
         </PrivateRoute>
       } />
+      
+      <Route path='/tareas/nueva' element={
+        <PrivateRoute>
+          <TareaForm />
+        </PrivateRoute>
+      } />
 
-      <Route path='/projects/:slug' element={<ProjectDetails />} />
-      <Route path='*' element={<Navigate to='/'/>} />
+      <Route path='*' element={<Navigate to='/' />} />
     </Routes>
   )
 }
